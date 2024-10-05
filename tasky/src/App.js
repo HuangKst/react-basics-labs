6c534eed049d83563c83a1ed924f458f3833e868
HuangKst <1300105758@qq.com>
2024-10-05 10:48:37 +0100

    Add task functionality

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Task from './components/Task';
 import React,{useState} from 'react';
 import AddTaskForm from './components/Form';
+import {v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -13,6 +14,28 @@ function App() {
     ]
   });
 
+  const [formState,setFormSate]= useState({
+    title:"",
+    description:"",
+    deadline:""
+  }); 
+
+  const formChangeHadnler = (event)=>{
+    let form = {...formState};
+    switch(event.target.name){
+      case "title":
+        form.title=event.target.value;
+        break;
+      case "description":
+        form.description=event.target.value;
+        break;
+      case "deadline":
+        form.deadline=event.target.value;
+    }
+    setFormSate(form);
+  }
+  console.log(formState);
+
   const doneHandler= (taskIndex)=>{
     const tasks=[...taskState.tasks];
     tasks[taskIndex].done=!tasks[taskIndex].done;//when click the button reverse the value
@@ -21,10 +44,22 @@ function App() {
 
   }
 
+  
+
   const deleteHandler = (taskIndex)=>{
     const tasks=[...taskState.tasks];
     //delete the element from the taskIndex and delete 1 element 
     tasks.splice(taskIndex,1);
+    setTaskState({tasks});
+  }
+
+  const formSubmitHandler = (event) =>{
+    event.preventDefault();
+    const tasks = [...taskState.tasks];
+    const form ={...formState};
+
+    form.id =uuidv4;
+    tasks.push(form);
     setTaskState({tasks});
   }
 
@@ -42,9 +77,10 @@ function App() {
           done={task.done}
           markDone={() => doneHandler(index)}
           deleteTask={() => deleteHandler(index)}
+
           />
       ))}
-      <AddTaskForm/>
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHadnler} />
     </div>
   );
   
